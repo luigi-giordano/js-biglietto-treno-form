328c26b143ad804dc5ab25b690ba4c09ce7f970d
luigi-giordano <luigi.giordano1992@gmail.com>
2024-11-25 18:16:04 +0100

    submit event

--- script.js
+++ script.js
@@ -3,15 +3,37 @@ const priceTicketKm = 0.21;
 const discountUnderAge = 20;
 const discountOverAge = 40;
 
+// Funzione per calcolare il prezzo
+function calcoloPrezzo(km, eta) {
+  const prezzoPerKm = 0.21;
+  let prezzoTotale = km * prezzoPerKm;
+
+  if (eta === "minorenne") {
+    prezzoTotale *= 0.8;
+} else if (eta === "anziano") {
+    prezzoTotale *= 0.6;
+}
+
+return prezzoTotale.toFixed(2);
+}
+
 //SELEZIONO GLI ELEMENTI DEL FORM
 const nomeCognome = document.getElementById('name');
-const kmPercorsi = document.getElementById('km');
+const kmPercorsi = parseInt(document.getElementById('km'));
 const etàPasseggero = document.getElementById('ageGroup');
 
 
 
 //SELEZIONO GLI ELEMENTI TARGET
-const nomePasseggero = document.getElementById('nome-passeggero')
+const nomePasseggero = document.getElementById('nome-passeggero');
+const offerta = document.getElementById('offerta');
+const carrozza = document.getElementById('carrozza');
+const codiceCp = document.getElementById('codice-cp');
+const costoBiglietto = document.getElementById('costo-biglietto');
+
+
+
+
 
 const form = document.querySelector('form');
 
@@ -21,6 +43,6 @@ form.addEventListener('submit', (event) =>{
   console.log(event);
   nomePasseggero.innerText = nomeCognome.value;
 
-
+  document.getElementById('card-biglietto').style.display = 'block';
+ 
 })
-
