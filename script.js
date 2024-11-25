a82cf08b805921af37d658d28290529c25417796
luigi-giordano <luigi.giordano1992@gmail.com>
2024-11-25 17:29:20 +0100

    first submit event

--- script.js
+++ script.js
@@ -3,19 +3,24 @@ const priceTicketKm = 0.21;
 const discountUnderAge = 20;
 const discountOverAge = 40;
 
+//SELEZIONO GLI ELEMENTI DEL FORM
 const nomeCognome = document.getElementById('name');
 const kmPercorsi = document.getElementById('km');
 const etàPasseggero = document.getElementById('ageGroup');
 
-const priceTicket = 0.21 * totalKm;
 
-let finalPrice = priceTicket * priceTicketKm;
 
-if (agePassenger < 18){
-  const priceUnderAge = priceTicket * (1 - discountUnderAge / 100);
-  finalPrice = priceUnderAge
-} else if (agePassenger > 65){
-  const priceOverAge = priceTicket * (1 - discountOverAge / 100);
-  finalPrice = priceOverAge
-}
-console.log('Il prezzo finale sarà', finalPrice.toFixed(2));
+//SELEZIONO GLI ELEMENTI TARGET
+const nomePasseggero = document.getElementById('nome-passeggero')
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (event) =>{
+  //INIBISCO L'INVIO DEL FORM
+  event.preventDefault();
+  console.log(event);
+  nomePasseggero.innerText = nomeCognome.value;
+
+
+})
+
