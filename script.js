9e49a9877d3f9aaede8d56b54c6edcba143e983a
luigi-giordano <luigi.giordano1992@gmail.com>
2024-11-25 15:33:18 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+/*VARIABILI*/
+const priceTicketKm = 0.21;
+const discountUnderAge = 20;
+const discountOverAge = 40;
+
+const priceTicket = 0.21 * totalKm;
+
+let finalPrice = priceTicket * priceTicketKm;
+
